eb1e9d42dfb7b276fbb12f06207c5e114086433b
raulnabarret <raulnabarret@gmail.com>
2016-07-09 16:15:07 -0500

    Imprimir en index.html listado de series usando el API de TVmaze

--- js/index.js
+++ js/index.js
@@ -11,10 +11,31 @@ $(document).ready(function () {
 
  	})
 
+ 	var template = '<article class="tv-show">'+
+						'<div class="left img-container">'+
+							'<img src=":img:" alt=":img alt:"></img>'+
+						'</div>'+
+						'<div class="right info">'+
+							'<h1>:name:</h1>'+
+							'<p>:summary:</p>'+
+						'</div>'+
+					'</article>'
+
  	$.ajax({
  		url: 'http://api.tvmaze.com/shows',
  		success: function (data, textStatus, xhr) {
- 			console.log(data)
+
+ 			$tvShowsContainer = $('#app-body').find('.tv-shows')
+ 			
+ 			data.forEach(function (show) {
+
+ 				var article = template.replace(':name:', show.name)
+ 										.replace(':summary:', show.summary)
+ 										.replace(':img:', show.image.medium)
+ 										.replace(':img alt:', show.name + " cover")
+ 			
+ 				$tvShowsContainer.append($(article))
+ 			})
  		}
  	})
 

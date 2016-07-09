cf35b2c72b4772007f9235d1a41b6df016c950ef
raulnabarret <raulnabarret@gmail.com>
2016-07-08 23:30:22 -0500

    Leer en consola entradas de la búsqueda del formulario

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 $(document).ready(function () {
 
+ /*
+	Submit search form
+ */
+ 	$("#app-body").find('form').submit(function (ev) {
+ 		ev.preventDefault()
+ 		console.log('hola')
+ 	}) 
 
-
+ 	
 })

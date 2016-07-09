40ee7a57d936d1c4c1821676897e286ef4931934
raulnabarret <raulnabarret@gmail.com>
2016-07-09 15:48:40 -0500

    Conectar con API de TvMaze a través de AJAX

--- js/index.js
+++ js/index.js
@@ -1,14 +1,23 @@
 $(document).ready(function () {
 
- /*
-	Submit search form
- */
- 	$("#app-body").find('form').submit(function (ev) {
- 		ev.preventDefault()
+ 	/*
+		Submit search form
+	 */
 
+ 	$("#app-body").find('form').submit(function (ev) {
+ 		
+ 		ev.preventDefault()
  		var query = $(this).find('input[type=text]').val()
 
- 	}) 
+ 	})
+
+ 	$.ajax({
+ 		url: 'http://api.tvmaze.com/shows',
+ 		success: function (data, textStatus, xhr) {
+ 			console.log(data)
+ 		}
+ 	})
+
 
  	
 })
